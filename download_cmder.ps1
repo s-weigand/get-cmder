626dd746c2ff6b1b3d904ba332ab71b7dcfa9694
s-weigand <s.weigand.phy@gmail.com>
2019-11-11 20:03:12 +0100

    added first copying of custom configs

--- download_cmder.ps1
+++ download_cmder.ps1
@@ -1,10 +1,15 @@
 $download_url = "https://github.com/cmderdev/cmder/releases/download/v1.3.12/cmder.zip"
 $download_path = Join-Path (Resolve-Path .\).Path "cmder.zip"
 $unpack_path = Join-Path (Resolve-Path .\).Path "cmder/"
+$custom_lua_conf_path_src = Join-Path (Resolve-Path .\).Path "custom_configs/clink.lua"
+$custom_git_promt_conf_path_src = Join-Path (Resolve-Path .\).Path "custom_configs/git-prompt.sh"
+$custom_lua_conf_path_dest = Join-Path $unpack_path "vendor/clink.lua"
+$custom_git_promt_conf_path_dest = Join-Path $unpack_path "vendor/git-for-windows/etc/profile.d/git-prompt.sh"
 
-# $client = new-object System.Net.WebClient
-# $client.DownloadFile($download_url, $download_path)
-
+function force_copy {
+    param ( [string]$src, [string]$dest)
+    Copy-Item $src $dest -force
+}
 function delete_file {
     param([string]$path)
     if (Test-Path $path) {
@@ -36,3 +41,7 @@ Download $download_url $download_path
 Unzip $download_path $unpack_path
 "Removing downloaded file"
 delete_file $download_path
+
+"copy custom configs"
+force_copy $custom_lua_conf_path_src $custom_lua_conf_path_dest
+force_copy $custom_git_promt_conf_path_src $custom_git_promt_conf_path_dest
