476bc87c2c045c973f874ccc112399d39802bdeb
s-weigand <s.weigand.phy@gmail.com>
2020-01-09 17:12:11 +0100

    commented clean function artifact from development

--- powershell_files/download_cmder.ps1
+++ powershell_files/download_cmder.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot\function_definitions.ps1
 
-clean
+# clean
 download_and_unzip $download_cmder_url $cmder_unpack_path
 download_and_unzip $download_make_url $make_unpack_path
 copy_make $make_dest_path
